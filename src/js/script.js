62474b0323770fee1c77da40853398cd86ea8c42
Adrian <klimasa98@gmail.com>
2023-10-01 19:43:25 -0400

    created sidebar for nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
-const hamburger  = document.querySelector('.hamburger')
+const hamburger = document.querySelector('.hamburger');
+const mobile = document.querySelector('.nav-bar__mobile-menu');
 
-hamburger.addEventListener('click', ()=>{
-    hamburger.classList.toggle('hamburger--active')
-})
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('hamburger--active');
+	mobile.classList.toggle('show-nav-bar');
+});

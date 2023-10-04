96447b05d84d31ae3be1f75f8cc068f68c41de30
Adrian <klimasa98@gmail.com>
2023-10-04 17:29:34 -0400

    created footer for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const mobile = document.querySelector('.nav-bar__mobile-menu');
 const allNavItems = document.querySelectorAll('.nav-item')
+const currYear = document.querySelector('.year')
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('hamburger--active');
@@ -11,3 +12,5 @@ allNavItems.forEach(el => el.addEventListener('click', () =>{
     mobile.classList.remove('show-nav-bar')
     hamburger.classList.remove('hamburger--active');
 }))
+
+currYear.textContent = new Date().getFullYear()
